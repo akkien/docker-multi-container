19ae8bd0dbd4e8b39fb6f68f9f90d82ba15be4f6
Le Trung Kien <akkien99@gmail.com>
2021-08-12 14:38:11 +0700

    fix package version

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 class App extends Component {
   render() {
@@ -13,8 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/">Home</Link> <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
